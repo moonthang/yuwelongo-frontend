d24adb5528528ce7901f11266476502d185a6c95
moonthang <burgosmiguel50@gmail.com>
2025-11-13 13:57:03 -0500

    Fixes changes from the previous commit that were not uploaded 0.1

--- src/pages/public/juegoPage/JuegoPreviewPage.js
+++ src/pages/public/juegoPage/JuegoPreviewPage.js
@@ -42,6 +42,12 @@ export default function JuegoPreviewPage() {
             <i className="bi bi-clock"></i>
             <span>5-10 min</span>
           </div>
+          <div className="juego-ranking-tag">
+            <Link to="/ranking" className="juego-ranking-content">
+              <i className="bi bi-stars"></i>
+              <span>Ver Ranking</span>
+            </Link>
+          </div>
         </div>
         <section className="juego-preview-section">
           <h3>Objetivo del Juego</h3>
